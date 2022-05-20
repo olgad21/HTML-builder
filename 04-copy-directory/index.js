116d994b8a3a22b0e5c0da69156834feae03f08d
Olga <olga.dashuk21@gmail.com>
2022-05-19 20:17:57 -0400

    feat: implement copying directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
+const path = require('path');
+const fs = require('fs/promises');
 
+const newDirPath = path.join(__dirname, path.sep, 'files-copy');
+const dirPath = path.join(__dirname, path.sep, 'files');
+
+fs.readdir(dirPath)
+  .then(async(files) => {
+    await fs.mkdir(newDirPath, {recursive: true})
+      .then(async() => {
+        for (let file of files){
+          const filePath = path.join(dirPath, file);
+          const newFilePath = path.join(newDirPath, file);
+          await fs.copyFile(filePath, newFilePath);
+        }
+      });
+  });
+   
