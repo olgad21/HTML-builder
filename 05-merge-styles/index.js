5a5f2280cefb6c42a8f2766d25e33cb453ca8591
Olga <olga.dashuk21@gmail.com>
2022-05-19 23:41:47 -0400

    feat: implement merging styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,36 @@
+const fsPromises = require('fs/promises');
+const fs = require('fs');
+const path = require('path');
+
+const stylesPath = path.join(__dirname, path.sep, 'styles');
+
+fsPromises.readdir(stylesPath, { withFileTypes: true })
+  .then(async(files) => {
+    const stylesData = [];
+    const writeStream = fs.createWriteStream(path.join(__dirname, path.sep, 'project-dist/', 'bundle.css'));
+    
+    for (let file of files) {
+      if (file.isFile() && file.name.endsWith('.css')){
+        await initReadableStream(stylesData, file)
+          .then(stylesData => {
+            for (let styleFile of stylesData){
+              writeStream.write(styleFile);
+            }
+          });
+      }
+    }
+  });
+
+function initReadableStream(stylesData, file) {
+  return new Promise(resolve => {
+    const readStream = fs.createReadStream(path.join(stylesPath, file.name));
+    //const writeStream = fs.createWriteStream(path.join(__dirname, path.sep, 'project-dist/', 'bundle.css'));
+
+    //readStream.pipe(writeStream);
+
+    readStream.on('data', (chunk) => {
+      stylesData.push(chunk.toString());
+    });
+    readStream.on('end', () => resolve(stylesData));
+  });
+}
