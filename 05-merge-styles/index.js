65f51c9903b5b9e612aa5e7e66a7f45917b0d8b6
Olga <olga.dashuk21@gmail.com>
2022-05-25 13:30:57 -0400

    fix: css files merged several times in task 5

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -6,17 +6,15 @@ const stylesPath = path.join(__dirname, path.sep, 'styles');
 
 fsPromises.readdir(stylesPath, { withFileTypes: true })
   .then(async(files) => {
-    const stylesData = [];
+    let stylesData;
     const writeStream = fs.createWriteStream(path.join(__dirname, path.sep, 'project-dist/', 'bundle.css'));
     
     for (let file of files) {
       if (file.isFile() && file.name.endsWith('.css')){
         await initReadableStream(stylesData, file)
           .then(stylesData => {
-            for (let styleFile of stylesData){
-              styleFile = `${styleFile}\n`;
-              writeStream.write(styleFile);
-            }
+            stylesData = `${stylesData}\n`;
+            writeStream.write(stylesData);
           });
       }
     }
@@ -25,12 +23,9 @@ fsPromises.readdir(stylesPath, { withFileTypes: true })
 function initReadableStream(stylesData, file) {
   return new Promise(resolve => {
     const readStream = fs.createReadStream(path.join(stylesPath, file.name));
-    //const writeStream = fs.createWriteStream(path.join(__dirname, path.sep, 'project-dist/', 'bundle.css'));
-
-    //readStream.pipe(writeStream);
 
     readStream.on('data', (chunk) => {
-      stylesData.push(chunk.toString());
+      stylesData = chunk.toString();
     });
     readStream.on('end', () => resolve(stylesData));
   });
