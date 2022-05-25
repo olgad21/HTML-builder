488e2e695b8798601a342ac8406aa10296180e37
Olga <olga.dashuk21@gmail.com>
2022-05-25 11:08:27 -0400

    fix: files-copy folder does not update if file is deleted

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,18 +1,36 @@
 const path = require('path');
-const fs = require('fs/promises');
+const fsPromises = require('fs/promises');
+const fs = require('fs');
 
 const newDirPath = path.join(__dirname, path.sep, 'files-copy');
 const dirPath = path.join(__dirname, path.sep, 'files');
 
-fs.readdir(dirPath)
+fsPromises.readdir(dirPath)
   .then(async(files) => {
-    await fs.mkdir(newDirPath, {recursive: true})
-      .then(async() => {
-        for (let file of files){
-          const filePath = path.join(dirPath, file);
-          const newFilePath = path.join(newDirPath, file);
-          await fs.copyFile(filePath, newFilePath);
-        }
-      });
+    fs.access(newDirPath, fs.constants.F_OK, function(error) {
+      if (error) {
+        fsPromises.mkdir(newDirPath, {recursive: true})
+          .then(async() => {
+            for (let file of files){
+              const filePath = path.join(dirPath, file);
+              const newFilePath = path.join(newDirPath, file);
+              await fsPromises.copyFile(filePath, newFilePath);
+            }
+          });
+      } else {
+        fsPromises.rm(newDirPath, {recursive: true})
+          .then(async() => {
+            await fsPromises.mkdir(newDirPath, {recursive: true})
+              .then(async() => {
+                for (let file of files){
+                  const filePath = path.join(dirPath, file);
+                  const newFilePath = path.join(newDirPath, file);
+                  await fsPromises.copyFile(filePath, newFilePath);
+                }
+              });
+          });
+            
+      }
+    }); 
   });
    
